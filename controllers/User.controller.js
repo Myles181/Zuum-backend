6e1e7daa22fc81d84db0c2d65ed487944c360aee
Kazutech1 <umehemmanuel22@gmail.com>
2025-02-26 16:37:11 -0800

    Implemented Google Auth feature

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -39,7 +39,7 @@ exports.login = async (req, res) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALL_BACK,
     passReqToCallback: true
   },
   async function(request, accessToken, refreshToken, profile, done) {
