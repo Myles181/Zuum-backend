c2049770d918e435d799b3cb980f00db50d0dade
Myles181 <cyrile450@gmail.com>
2025-03-13 19:18:33 -0700

    Feat: Add Identity field

--- controllers/Auth.controller.js
+++ controllers/Auth.controller.js
@@ -229,14 +229,6 @@ exports.resendOtp = async (req, res) => {
         await saveOtp(email, newOtp);
 
         // Send OTP via Email
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-
         const mailOptions = {
             from: process.env.EMAIL_USER,
             to: email,
